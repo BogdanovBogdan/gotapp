392b245c47cb13d75c067a544bf15fa30ec6ecac
Bogdan Bogdanov <bogdanov_bogdanv@mail.ru>
2021-07-10 18:49:20 +0300

    Move character template to separate view

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./randomChar.css";
 import gotService from '../../api';
+import View from './View';
 
 export default class RandomChar extends Component {
   constructor() {
@@ -20,28 +21,10 @@ export default class RandomChar extends Component {
   }
   
   render() {
-    const { name, gender, born, died, culture } = this.state;
+    const { character } = this.state;
     return (
       <div className="random-block rounded">
-        <h4>Random Character: { name }</h4>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Gender </span>
-            <span>{ gender }</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Born </span>
-            <span>{ born }</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Died </span>
-            <span>{ died }</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Culture </span>
-            <span>{ culture }</span>
-          </li>
-        </ul>
+        <View character={character} />
       </div>
     );
   }
