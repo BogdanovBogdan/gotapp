0d28fe3aeb1912d8ff14045b94ddefc2af52192f
Bogdan Bogdanov <bogdanov_bogdanv@mail.ru>
2021-07-10 18:59:04 +0300

    Add condition displaying spinner untill data of character is load

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./randomChar.css";
 import gotService from '../../api';
 import View from './View';
+import Spinner from '../Spinner';
 
 export default class RandomChar extends Component {
   constructor() {
@@ -12,19 +13,20 @@ export default class RandomChar extends Component {
   gotService = new gotService();
   state = {
     character: {},
+    loading: true,
   }
   
   updateCharacter() {
     const id = Math.floor(Math.random() * 100 + 25);
     this.gotService.getCharacter(id)
-      .then((character) => { this.setState({character}); });
+      .then((character) => { this.setState({character, loading: false}); });
   }
   
   render() {
-    const { character } = this.state;
+    const { character, loading } = this.state;
     return (
       <div className="random-block rounded">
-        <View character={character} />
+        { loading ? <Spinner /> : <View character={character}/> }
       </div>
     );
   }
