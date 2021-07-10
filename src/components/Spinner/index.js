6e40a898f160b399c7eea961c68a7f6f1079e2e9
Bogdan Bogdanov <bogdanov_bogdanv@mail.ru>
2021-07-10 18:22:43 +0300

    Add spinner

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css';
+
+export default function Spinner() {
+  return (
+    <div class="loadingio-spinner-spin-lv7l26fsego">
+      <div class="ldio-3jj86itmvzp">
+        <div><div></div></div><div><div></div></div><div><div></div></div>
+        <div><div></div></div><div><div></div></div><div><div></div></div>
+        <div><div></div></div><div><div></div></div>
+      </div>
+    </div>
+  )
+}
