b7724d601ecfc2a9965b93883922e1af949c4342
Bogdan Bogdanov <bogdanov_bogdanv@mail.ru>
2021-07-17 10:49:40 +0300

    Add oportunity update of character

--- src/components/charDetails/charDetails.js
+++ src/components/charDetails/charDetails.js
@@ -1,26 +1,62 @@
 import React, { Component } from "react";
 import "./charDetails.css";
+import gotService from "../../api";
+
 export default class CharDetails extends Component {
+  constructor() {
+    super();
+    this.gotService = new gotService();
+    this.state = {
+      character: null,
+    };
+  }
+
+  componentDidMount() {
+    this.updateChar();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.selectedCharId !== this.props.selectedCharId) this.updateChar();
+  }
+
+  updateChar() {
+    if (!this.props.selectedCharId) return null;
+    const { selectedCharId } = this.props;
+    this.gotService.getCharacter(selectedCharId).then((char) => {
+      this.setState({ character: char });
+    });
+  }
+
   render() {
+    if (!this.state.character) {
+      return (
+        <div class="char-details rounded">
+          <h4>Character not selected</h4>
+        </div>
+      );
+    }
+
+    const { name, gender, born, died, culture } = this.state.character;
+
     return (
       <div className="char-details rounded">
-        <h4>John Snow</h4>
+        <h4>{name}</h4>
         <ul className="list-group list-group-flush">
           <li className="list-group-item d-flex justify-content-between">
             <span className="term">Gender</span>
-            <span>male</span>
+            <span>{gender || "—"}</span>
           </li>
           <li className="list-group-item d-flex justify-content-between">
             <span className="term">Born</span>
-            <span>1783</span>
+            <span>{born || "—"}</span>
           </li>
           <li className="list-group-item d-flex justify-content-between">
             <span className="term">Died</span>
-            <span>1820</span>
+            <span>{died || "—"}</span>
           </li>
           <li className="list-group-item d-flex justify-content-between">
             <span className="term">Culture</span>
-            <span>First</span>
+            <span>{culture || "—"}</span>
           </li>
         </ul>
       </div>
